bf8ccbd81a0a2e3f506695e01fcdb756ffe533e0
Dan Roberts <danroberts728@gmail.com>
2025-10-05 13:23:19 -0500

    Finalizing first draft

--- src/config.js
+++ src/config.js
@@ -1,3 +1,18 @@
 // Centralized config
 export const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1eIoInG9n4CuX1Y6GoR5gWfoj1LBiqe2g0i455bN67yNWkptcJvyz20cgIWhmMGITqmP7od0wvVoH/pub?gid=852319562&single=true&output=csv";
 export const RECIPES_URL = "./assets/data/recipes.json";
+
+export const DEFAULT_TBD = "TBD";
+
+export const ERRORSTR_RECIPE_FETCH = "No recipes loaded:";
+
+export const STR_NO_RECIPE = "Recipe coming soon.";
+
+export const ELEMENTID_RECIPE_MODAL = "recipe-modal";
+export const ELEMENTID_RECIPE_TITLE = "recipe-title";
+export const ELEMENTID_RECIPE_PHOOTO = "recipe-photo";
+export const ELEMENTID_RECIPE_INGREDIENTS = "recipe-ingredients";
+export const ELEMENTID_RECIPE_INSTRUCTIONS = "recipe-instructions";
+export const ELEMENTID_RECIPE_NOTES = "recipe-notes";
+export const ELEMENTID_INGREDIENTS_TITLE = "ingredients-title";
+export const ELEMENTID_INSTRUCTIONS_TITLE = "instructions-title";
